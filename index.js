92e6c417608a46003dddcdcd101dc5dfd2e077c3
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-05-11 01:31:33 +0600

    single volunteer post get request done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -29,6 +29,14 @@ async function run() {
 
     app.get('/volunteers-upcoming', async (req, res) => {
       const result = await volunteerCollection.find().limit(6).toArray();
+      res.send(result);
+    });
+
+    app.get('/volunteers/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const cursor = { _id: new ObjectId(id) };
+      const result = await volunteerCollection.findOne(cursor);
       res.send(result);
     });
 
