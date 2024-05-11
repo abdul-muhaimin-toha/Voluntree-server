313a9ad7f746ab0d26bf4606b1a0188bfb8ce87c
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-05-11 18:56:05 +0600

    delete post doned

--- index.js
+++ index.js
@@ -115,6 +115,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/volunteers/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerCollection.deleteOne(query);
+      res.send(result);
+    });
+
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     );
