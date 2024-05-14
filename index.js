cdc6afb8569b83a462a52fa51dbfec4361650015
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-05-14 16:35:33 +0600

    deployment done v.3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: ['https://voluntree-go.netlify.app', 'http://localhost:5173'],
+  origin: ['https://voluntree-go.netlify.app'],
   credentials: true,
 };
 
